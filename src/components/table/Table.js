0010a26bcbc5b1d0a9683a32406300579ca4578b
serhii.rubets <serhii.rubets@playtika.com>
2020-05-15 06:01:13 +0300

    Add excel resize

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,25 @@
 import {ExcelComponent} from '@core/ExcelComponent';
 import {createTable} from '@/components/table/table.template';
+import {resizeHandler} from '@/components/table/table.resize';
+import {shouldResize} from '@/components/table/table.helpers';
 
 export class Table extends ExcelComponent {
   static className = 'excel__table';
 
+  constructor($root) {
+    super($root, {
+      name: 'Table',
+      listeners: ['mousedown'],
+    });
+  }
+
+  onMousedown(e) {
+    if (shouldResize(e)) {
+      resizeHandler(e, this.$root);
+    }
+  }
+
   toHTML() {
-    return createTable(25);
+    return createTable(250);
   }
 }
---
+++ src/components/table/table.resize.js
@@ -0,0 +1,42 @@
+import {$} from '@core/dom';
+
+export function resizeHandler(e, $root) {
+  const $resizer = $(e.target);
+  const $parent = $resizer.closest('[data-type="resizable"]');
+  const coords = $parent.getCoords();
+  const index = $parent.data.col;
+  const cells = $root.findAll(`[data-col="${index}"]`);
+  const type = $resizer.data.resize;
+  const sideProp = type === 'col' ? 'bottom' : 'right';
+  let value = 0;
+
+  $resizer.css({opacity: 1, [sideProp]: '-5000px'});
+
+  document.onmousemove = (e) => {
+    if (type === 'col') {
+      const delta = e.pageX - coords.right;
+      value = coords.width + delta;
+
+      $resizer.css({right: -delta + 'px'});
+    } else {
+      const delta = e.pageY - coords.bottom;
+      value = coords.height + delta;
+
+      $resizer.css({bottom: -delta + 'px'});
+    }
+  };
+
+  document.onmouseup = () => {
+    $resizer.css({opacity: 0, bottom: '0', right: 0});
+
+    if (type === 'col') {
+      $parent.css({width: value + 'px'});
+      cells.forEach(el => el.style.width = value + 'px');
+    } else {
+      $parent.css({height: value + 'px'});
+    }
+
+    document.onmouseup = null;
+    document.onmousemove = null;
+  };
+}
